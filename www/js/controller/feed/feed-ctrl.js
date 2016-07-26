7fc73e8dbbdb378741e40009d83b51daafe9b8fd
Deepank Agarwal <deepank411@gmail.com>
2016-07-26 11:29:04 +0530

    working on fab feed 4

--- www/js/controller/feed/feed-ctrl.js
+++ www/js/controller/feed/feed-ctrl.js
@@ -2,6 +2,7 @@ app.controller('FeedCtrl', ['$scope', '$timeout', function($scope, $timeout){
 
 	console.log('test');
 	$scope.events2 = [];
+	$scope.moreMessages = true;
 
 	// $scope.liked = false;
 	// console.log($scope.liked);
@@ -25,18 +26,30 @@ app.controller('FeedCtrl', ['$scope', '$timeout', function($scope, $timeout){
 	$scope.loadMore = function(){
 
 		if($scope.events2.length > 0){
-			console.log($scope.bottomKey);
-			db.ref("blogs").orderByKey().limitToFirst(5).endAt($scope.bottomKey).once("value", function(snap){
-				console.log(snap.val());
-				console.log(Object.keys(snap.val())[0]);
-				$scope.bottomKey = Object.keys(snap.val())[0];
-				angular.forEach(snap.val(), function(value, key){
-					value.introduction = value.introduction.replace(/#(\w+)(?!\w)/g,'<a href="#/tag/$1">#$1</a>');
-					$scope.events2.push(value);
-					console.log($scope.events2);
+			if($scope.moreMessages == true){
+				console.log($scope.bottomKey);
+				db.ref("blogs").orderByKey().limitToFirst(5).endAt($scope.bottomKey).once("value", function(snap){
+					console.log(snap.numChildren(), $scope.moreMessages);
+					if(snap.numChildren() == 1){
+						$scope.moreMessages = false;
+						$scope.$broadcast('scroll.infiniteScrollComplete');
+					}
+					else{
+						console.log(snap.val());
+						console.log(Object.keys(snap.val())[0]);
+						$scope.bottomKey = Object.keys(snap.val())[0];
+						angular.forEach(snap.val(), function(value, key){
+							value.introduction = value.introduction.replace(/#(\w+)(?!\w)/g,'<a href="#/tag/$1">#$1</a>');
+							$scope.events2.push(value);
+							console.log($scope.events2);
+						});
+						$scope.$broadcast('scroll.infiniteScrollComplete');
+					}
 				});
+			}
+			else{
 				$scope.$broadcast('scroll.infiniteScrollComplete');
-			});
+			}
 		}
 		else if($scope.events2.length == 0){
 			db.ref().child("blogs").limitToLast(5).once("value", function(snapshot){

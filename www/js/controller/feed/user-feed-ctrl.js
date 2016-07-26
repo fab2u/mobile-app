4c1d628c3bebcc0ab4d2b2149299ee4e44237b74
Deepank Agarwal <deepank411@gmail.com>
2016-07-26 22:56:23 +0530

    user-feed integrated 1

--- www/js/controller/feed/user-feed-ctrl.js
+++ www/js/controller/feed/user-feed-ctrl.js
@@ -5,18 +5,57 @@ app.controller("userFeedCtrl", ['$scope', '$timeout', '$stateParams', function($
    $scope.img_hash = md5($scope.email);
    jdenticon.update("#identicon", $scope.img_hash);
    var uid = window.localStorage.uid;
-   var ref = db.ref("users/data/"+uid +"/blogs");
-   ref.once("value", function(snapshot){
-      $scope.blogArr = [];
-      console.log(snapshot.val());
-      for(var i in snapshot.val()){
-         var blogData = db.ref("blogs/" + i);
-         blogData.once("value", function(snap){
-            single_blog = snap.val();
-            single_blog.introduction = single_blog.introduction.replace(/#(\w+)(?!\w)/g,'<a href="#/tag/$1">#$1</a>');
-            console.log(single_blog);
-				$scope.blogArr.push(single_blog);
+   $scope.blogIdList = {};
+   $scope.moreMessagesScroll = true;
+
+   $scope.loadMore = function(){
+      console.log(Object.keys($scope.blogIdList).length);
+		if(Object.keys($scope.blogIdList).length > 0){
+			console.log($scope.bottomKey);
+         db.ref("users/data/"+uid+"/blogs").orderByKey().limitToFirst(5).endAt($scope.bottomKey).once("value", function(snap){
+            console.log(snap.val());
+            if(snap.numChildren() == 1){
+					$scope.moreMessagesScroll = false;
+					$scope.$broadcast('scroll.infiniteScrollComplete');
+				}
+				else{
+					$scope.bottomKey = Object.keys(snap.val())[0];
+					for(var i in snap.val()){
+						// console.log(i); // i is the key of blogs object or the id of each blog
+						var blogData = db.ref().child("blogs").child(i);
+						blogData.once("value", function(snap){ //access individual blog
+							// console.log(snap.val());
+							single_blog = snap.val();
+							single_blog.introduction = single_blog.introduction.replace(/#(\w+)(?!\w)/g,'<a href="#/tag/$1">#$1</a>');
+							$scope.blogArr.push(single_blog);
+							// console.log($scope.blogArr);
+						});
+					}
+					$scope.$broadcast('scroll.infiniteScrollComplete');
+				}
+         })
+      }
+      else if(Object.keys($scope.blogIdList).length == 0){
+         db.ref("users/data/"+uid +"/blogs").limitToLast(5).once("value", function(snapshot){
+            console.log(snapshot.val());
+            $scope.blogIdList = snapshot.val();
+				console.log($scope.blogIdList);
+				$scope.bottomKey = Object.keys($scope.blogIdList)[0];
+            $scope.blogArr = [];
+            for(var i in snapshot.val()){
+               var blogData = db.ref("blogs/" + i);
+               blogData.once("value", function(snap){
+                  single_blog = snap.val();
+                  single_blog.introduction = single_blog.introduction.replace(/#(\w+)(?!\w)/g,'<a href="#/tag/$1">#$1</a>');
+                  console.log(single_blog);
+      				$scope.blogArr.push(single_blog);
+               });
+            }
          });
       }
-   });
+   }
+   $scope.$on('$stateChangeSuccess', function() {
+   	$scope.loadMore();
+	});
+
 }]);

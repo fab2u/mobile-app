a161e7b032b9f87e34f37688e24e3d3dde9f6aa3
sonam gupta <sonam.gupta30jan@gmail.com>
2016-08-20 10:49:20 +0530

    social sharing for referral code implemented

--- www/js/controller/user/refer-ctrl.js
+++ www/js/controller/user/refer-ctrl.js
@@ -1,8 +1,38 @@
-app.controller('ReferCtrl', ['$scope', '$state', function($scope, $state){
+app.controller('ReferCtrl', ['$scope', '$state','$cordovaSocialSharing', function($scope, $state,$cordovaSocialSharing){
 
-	firebase.database
+	firebase.database().ref('/users/data/'+window.localStorage.getItem('uid')).once('value',function(response){
+		console.log("response for referal code",response.val().myReferralCode);
+		$scope.myReferralCode = response.val().myReferralCode;
+	});
+
+
 	$scope.showReferDetails = function(){
 		$state.go('referralDetails');
-	}
+	};
+
+	// Referral code sharing over whatsApp//
+
+	$scope.WhatsApp = function () {
+		$cordovaSocialSharing
+			.shareViaWhatsApp(message, image, link)
+			.then(function (result) {
+				// Success!
+			}, function (err) {
+				// An error occurred. Show a message to the user
+			});
+	};
+
+	// Referral code sharing over Facebook//
+
+	$scope.Facebook = function () {
+		$cordovaSocialSharing
+			.shareViaFacebook(message, image, link)
+			.then(function (result) {
+				// Success!
+			}, function (err) {
+				// An error occurred. Show a message to the user
+			});
+	};
+
 
 }]);

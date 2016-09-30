469c5456fd67f1b7d34fd257ac6f22ead124b22f
Deepank Agarwal <deepank411@gmail.com>
2016-09-30 13:47:13 +0530

    image on click goes full screen, stable

--- www/js/controller/feed/feed-ctrl.js
+++ www/js/controller/feed/feed-ctrl.js
@@ -1,6 +1,44 @@
-app.controller('FeedCtrl', ['$scope', '$timeout', '$location', '$ionicLoading', '$cordovaSocialSharing', '$ionicPopup', function($scope, $timeout, $location, $ionicLoading, $cordovaSocialSharing, $ionicPopup){
+app.controller('FeedCtrl', ['$scope', '$timeout', '$location', '$ionicLoading', '$cordovaSocialSharing', '$ionicPopup', '$ionicModal', function($scope, $timeout, $location, $ionicLoading, $cordovaSocialSharing, $ionicPopup, $ionicModal){
 
 	$ionicLoading.show();
+
+	// ----------------------------------------------------------------------
+	$ionicModal.fromTemplateUrl('templates/feed/image-modal.html', {
+		scope: $scope,
+		animation: 'slide-in-up'
+	}).then(function(modal) {
+		$scope.modal = modal;
+	});
+
+	$scope.openModal = function() {
+		$scope.modal.show();
+	};
+
+	$scope.closeModal = function() {
+		$scope.modal.hide();
+	};
+
+	//Cleanup the modal when we're done with it!
+	$scope.$on('$destroy', function() {
+		$scope.modal.remove();
+	});
+	// Execute action on hide modal
+	$scope.$on('modal.hide', function() {
+		// Execute action
+	});
+	// Execute action on remove modal
+	$scope.$on('modal.removed', function() {
+		// Execute action
+	});
+	$scope.$on('modal.shown', function() {
+		console.log('Modal is shown!');
+	});
+
+	$scope.showImage = function(source) {
+		$scope.imageSrc = source;
+		$scope.openModal();
+	}
+	// ----------------------------------------------------------------------
 
 	console.log('test');
 	$scope.uid = window.localStorage.getItem("uid");

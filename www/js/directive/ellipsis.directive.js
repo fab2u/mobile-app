69408f5aaedc1dced6ee24db2507d5812e969fae
Deepank Agarwal <deepank411@gmail.com>
2016-10-01 16:31:13 +0530

    text truncation complete

--- www/js/directive/ellipsis.directive.js
+++ www/js/directive/ellipsis.directive.js
@@ -10,13 +10,10 @@ app.directive('ellipsis', [function () {
           // apply this code ONCE
           $scope.hasEllipsis = true;
           $(element).trunk8({
-            fill: ' <a id="read-more"><i class="icon ion-more"></i></a>', /*(Default: '&hellip;') The string to insert in place of the omitted text. This value may include HTML.*/
-            lines: 2, /*(Default: 1) The number of lines of text-wrap to tolerate before truncating. This value must be an integer greater than or equal to 1.*/
-            //side: 'right', /*(Default: 'right') The side of the text from which to truncate. Valid values include 'center', 'left', and 'right'.*/
-            tooltip: false, /*(Default: true) When true, the title attribute of the targeted HTML element will be set to the original, untruncated string. Valid values include true and false.*/
-            //width: 'auto', /*(Default: 'auto') The width, in characters, of the desired text. When set to 'auto', trunk8 will maximize the amount of text without spilling over.*/
-            parseHTML: true /*(Default: 'false') When true, parse and save html structure and restore structure in the truncated text.*/
-            //onTruncate /*(Callback): Called after truncation is completed.*/
+            fill: ' <a id="read-more"><i class="icon ion-more"></i></a>',
+            lines: 1,
+            tooltip: false,
+            parseHTML: true
           });
           $(element).on('click', '#read-more', function (event) {
             $(element).trunk8('revert').append(' <a id="read-less">less</a>');

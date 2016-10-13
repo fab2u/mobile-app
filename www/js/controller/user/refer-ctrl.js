7f4ff1eed86a1d58249ba18470c00ed1708222b2
sonam gupta <sonam.gupta30jan@gmail.com>
2016-10-13 18:04:02 +0530

    location issue and back button issue on menu page resolved

--- www/js/controller/user/refer-ctrl.js
+++ www/js/controller/user/refer-ctrl.js
@@ -17,7 +17,7 @@ app.controller('ReferCtrl',function($scope, $state,$cordovaSocialSharing,$ionicL
 
 	$scope.WhatsApp = function () {
 		$cordovaSocialSharing
-			.shareViaWhatsApp('Download the Fab2u app and use my referral code'+$scope.myReferralCode +'and get RS. 25 in wallet', '', '')
+			.shareViaWhatsApp('Download the Fab2u app and use my referral code  '+$scope.myReferralCode +'and get RS. 25 in wallet', '', '')
 			.then(function (result) {
 				// Success!
 			}, function (err) {
@@ -29,7 +29,7 @@ app.controller('ReferCtrl',function($scope, $state,$cordovaSocialSharing,$ionicL
 
 	$scope.Facebook = function () {
 		$cordovaSocialSharing
-			.shareViaFacebook('Download the Fab2u app and use my referral code'+$scope.myReferralCode +'and get RS. 25 in wallet', '', '')
+			.shareViaFacebook('Download the Fab2u app and use my referral code  '+$scope.myReferralCode +'and get RS. 25 in wallet', '', '')
 			.then(function (result) {
 				// Success!
 			}, function (err) {

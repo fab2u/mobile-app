13b43c4b71f01c122cfb135bb9325e1fee01c9fe
sonam gupta <sonam.gupta30jan@gmail.com>
2016-11-10 13:48:57 +0530

    legal dir code changed

--- www/js/controller/legal/contact-ctrl.js
+++ www/js/controller/legal/contact-ctrl.js
@@ -1,15 +1,32 @@
-app.controller('ContactCtrl', function($state, $scope,$cordovaToast) {
-    firebase.database().ref('city').once('value',function(response){
-        $scope.location_list = response.val();
-    });
+app.controller('ContactCtrl', function($state, $scope,$cordovaToast,$timeout,$ionicLoading) {
+
+    $scope.show = function() {
+        $ionicLoading.show();
+    };
+    $scope.show();
+    $timeout(function () {
+        $ionicLoading.hide();
+    }, 5000);
+
+     function locationOptions() {
+         $ionicLoading.show();
+         firebase.database().ref('city').once('value',function(response){
+            $scope.location_list = response.val();
+             $ionicLoading.hide();
+         });
+     }
+     locationOptions();
     $scope.user = {};
 
     $scope.query_options = ['HR','Appointment Booking','Marketing','Sales','Payment','Others'];
 
 
     $scope.submit_query = function(){
+        $ionicLoading.show();
         firebase.database().ref('contactUs').push($scope.user,function (error) {
             if(error){
+                $ionicLoading.hide();
+                $scope.user = {};
                 $cordovaToast
                     .show('Please try again!', 'long', 'center')
                     .then(function(success) {
@@ -19,6 +36,8 @@ app.controller('ContactCtrl', function($state, $scope,$cordovaToast) {
                     });
             }
             else{
+                $ionicLoading.hide();
+                $scope.user = {};
                 $cordovaToast
                     .show('Thanks for submitting your query. We will soon get back to you.', 'long', 'center')
                     .then(function(success) {
@@ -26,6 +45,7 @@ app.controller('ContactCtrl', function($state, $scope,$cordovaToast) {
                     }, function (error) {
                         // error
                     });
+                $state.go('app.home')
             }
         });
     };
